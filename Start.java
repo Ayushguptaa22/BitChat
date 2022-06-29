27d433411a7330446a1272821b430275da59f2dc
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-06-29 10:19:20 +0530

    Check java is working

---
+++ Start.java
@@ -0,0 +1,8 @@
+public class Start{
+    public static void main(String args[]){
+        System.out.println("Ayushsjhjndj");
+        int a=10,b=20;
+        System.out.println(a+b);
+    }
+    
+}
